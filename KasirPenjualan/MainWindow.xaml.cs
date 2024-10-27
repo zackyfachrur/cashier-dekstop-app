10d1b04ea2457afebf074bd3c61e0e14b7bfb5c5
zackyfachrur <mochzackyaja@gmail.com>
2024-10-27 18:55:00 +0700

    Cashier App Demo

---
+++ KasirPenjualan/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace KasirPenjualan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public LoginWindow LoginWindow { get; set; }
+        public RegisterWindow RegisterWindow { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow = new LoginWindow();
+            LoginWindow.Show();
+        }
+
+        private void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterWindow = new RegisterWindow();
+            RegisterWindow.Show();
+        }
+    }
+}
